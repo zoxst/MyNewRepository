a6da1bc99f5c522008a47874c202ca4be414e729
zoxst <jovanstankovic@jovan-stankovics-macbook-pro.local>
2017-02-02 00:54:31 +0100

    New feature GreatView added ...

---
+++ GitHubTesting/GreatView.swift
@@ -0,0 +1,21 @@
+//
+//  GreatView.swift
+//  GitHubTesting
+//
+//  Created by Jovan Stankovic on 2/2/17.
+//  Copyright © 2017 Geoinformatika. All rights reserved.
+//
+
+import UIKit
+
+class GreatView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
